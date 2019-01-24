8bd464070ff4869e52ba9ec0a39ca6e30d701419
Edoardo Pasca <edo.paskino@gmail.com>
2019-01-24 21:13:11 +0000

    bugfixes and more efficient loops

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+#   Copyright 2019 Edoardo Pasca
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 """
 Created on Wed Jan 23 14:33:56 2019
 
@@ -22,7 +35,6 @@ setup(
       name="DVC configurator app",
       description='CCPi DVC Configurator',
 	   version=cil_version,
-      # cmdclass = {'build_ext': build_ext},
 	   packages = {'ccpi','ccpi.apps'},
       ext_modules=cythonize(extensions)
 )
--- vtkutils.pyx
+++ vtkutils.pyx
@@ -8,11 +8,13 @@ Created on Wed Jan 23 14:37:57 2019
 
 def copyslices(indata, fromslice, zmin, zmax):
     cdef int x, y, z
+    cdef double val
     dims = indata.GetDimensions()
-    for x in range(dims[0]):
+    for z in range(zmin, zmax):
         for y in range(dims[1]):
-            for z in range(zmin, zmax):
+            for x in range(dims[0]):
                 if z != fromslice:
-                    val = indata.GetScalarComponentAsFloat(x,y,fromslice,0)
-                    indata.SetScalarComponentFromFloat(x,y,z,0,val)
+                    val = indata.GetScalarComponentAsDouble(x,y,fromslice,0)
+                    if val != 0
+                        indata.SetScalarComponentFromDouble(x,y,z,0,val)
     return 1
