bfaa6ce26ca75c07cd1ae247ccf8be53848dae2d
Edoardo Pasca <edo.paskino@gmail.com>
2019-01-24 16:31:49 +0000

    created conda package, refactoring

--- setup.py
+++ setup.py
@@ -4,10 +4,25 @@ Created on Wed Jan 23 14:33:56 2019
 
 @author: ofn77899
 """
-
+import os
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+
+cil_version=os.environ['CIL_VERSION']
+if  cil_version == '':
+    print("Please set the environmental variable CIL_VERSION")
+    sys.exit(1)
+
+sourcefiles = ["src/vtkutils.pyx"]
+
+extensions = [Extension("ccpi.apps.vtkutils", sourcefiles)]
 
 setup(
-    ext_modules = cythonize("vtkutils.pyx")
+      name="DVC configurator app",
+      description='CCPi DVC Configurator',
+	   version=cil_version,
+      # cmdclass = {'build_ext': build_ext},
+	   packages = {'ccpi','ccpi.apps'},
+      ext_modules=cythonize(extensions)
 )
