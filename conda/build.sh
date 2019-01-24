f92354ee7e3146e3e59d3d17e17687e12ac42f14
Edoardo Pasca <edo.paskino@gmail.com>
2019-01-24 21:27:47 +0000

    added build script for linux

--- conda/build.sh
+++ conda/build.sh
@@ -1,16 +1,11 @@
 
 mkdir "$SRC_DIR/ccpi"
-cp -rv "$RECIPE_DIR/../.." "$SRC_DIR/ccpi"
-cp -rv "$RECIPE_DIR/../../../Core" "$SRC_DIR/Core"
+cp -rv "$RECIPE_DIR/.." "$SRC_DIR/ccpi"
 
-cd $SRC_DIR
-##cuda=off
-cmake -G "Unix Makefiles" $RECIPE_DIR/../../../ -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB=$CONDA_PREFIX/lib -DLIBRARY_INC=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+cd $SRC_DIR/ccpi
 
-
-make install
 
 #$PYTHON setup-regularisers.py build_ext
-#$PYTHON setup-regularisers.py install
+$PYTHON setup.py install
 
 
