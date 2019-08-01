e4019ab0e4cb25d0250f4ab180b0e264fbb11bda
Edoardo Pasca <edo.paskino@gmail.com>
2019-08-01 16:22:24 +0100

    work in progress

--- wip/registration.py
+++ wip/registration.py
@@ -1,10 +1,10 @@
-from ccpi.viewer.CILViewer2D import CILViewer2D
+from ccpi.viewer.CILViewer2D import CILViewer2D, SLICE_ORIENTATION_XY, SLICE_ORIENTATION_XZ, SLICE_ORIENTATION_YZ
 import vtk
 from vtk.util.vtkAlgorithm import VTKPythonAlgorithmBase
 from vtk.util import numpy_support
 import glob
 import numpy
-
+import sys, os
 
 class cilToRGB(VTKPythonAlgorithmBase):
     '''vtkAlgorithm to crop a vtkPolyData with a Mask
@@ -198,30 +198,37 @@ class cilImageMathematics(VTKPythonAlgorithmBase):
                         ),
             (dims[0],dims[1],dims[2]), order='F'
         )
+        print (im1.min(), im2.min(), im1.max(), im2.max(), stack_array.min(), stack_array.max())
+        
         operation = self.__operation
         if operation == cilImageMathematics.ADD:
             func = self.add_to_float
+            np_op = numpy.add
         elif operation == cilImageMathematics.SUBTRACT:
             func = self.subtract_to_float
+            np_op = numpy.subtract
         elif operation == cilImageMathematics.MULTIPLY:
             func = self.multiply_to_float
+            np_op = numpy.multiply
         elif operation == cilImageMathematics.DIVIDE:
             func = self.divide_to_float
+            np_op = numpy.divide
         else:
             raise ValueError('Unsupported operation ', operation)
         print (func)
-        np_op = numpy.frompyfunc(func, 2, 1)
+        #np_op = numpy.frompyfunc(func, 2, 1)
         print (np_op)
         if self.__out_dtype == vtk.VTK_FLOAT:
             dtype = numpy.float32
         elif self.__out_dtype == vtk.VTK_DOUBLE:
             dtype = numpy.float64
-        print ("doing it")
-        a = numpy.asarray(np_op(im1,im2), dtype=dtype)
+        print ("doing it", np_op)
+        np_op(im1,im2, out=stack_array, dtype=dtype, order='F')
+        a = im1 - im2
         #stack_array[:] = numpy.asarray(np_op(im1,im2), dtype=dtype)
-        stack_array[:,:,:] = a
+        #stack_array[:,:,:] = a
         #print ("operation" , a)
-        print (im1.min(), im2.min(), im1.max(), im2.max(), a.min(), a.max())
+        print (im1.min(), im2.min(), im1.max(), im2.max(), stack_array.min(), stack_array.max(), "a", a.min(), a.max())
         # put the output in the out port
         output.ShallowCopy(stack)
         return 1 
@@ -243,7 +250,97 @@ def OnKeyPressEvent(interactor, event):
     translate.Update()
     subtract.Update()
     v.setInputData(subtract.GetOutput())
-    
+
+def OnKeyPressEventTransform(interactor, event):
+    '''https://gitlab.kitware.com/vtk/vtk/issues/15777'''
+    shift = 0.5
+    trans = [0,0,0]
+    if interactor.GetKeyCode() == "j":
+        trans[1] = shift
+    elif interactor.GetKeyCode() == "n":
+        trans[1] = -shift
+    elif interactor.GetKeyCode() == "b":
+        trans[0] = shift
+    elif interactor.GetKeyCode() == "m":
+        trans[0] = -shift
+    transform.Translate(*trans)
+    transform.Update()
+    translate.Update()
+    subtract.Update()
+    v.setInputData(subtract.GetOutput())
+
+def OnSliceEvent(interactor, event):
+    '''https://gitlab.kitware.com/vtk/vtk/issues/15777'''
+    # update the ROI 
+    sliceno = v.style.GetActiveSlice()
+    print (sliceno)
+    orientation = v.style.GetSliceOrientation()
+    extent = list(reader.GetOutput().GetExtent())
+    # Calculate the size of the ROI
+    if orientation == SLICE_ORIENTATION_XY:
+        print ("slice orientation : YZ")
+        extent[4] = sliceno
+        extent[5] = sliceno + 1
+    elif orientation == SLICE_ORIENTATION_XZ:
+        print ("slice orientation : YZ")
+        extent[2] = sliceno
+        extent[3] = sliceno + 1
+
+    elif orientation == SLICE_ORIENTATION_YZ:
+        print ("slice orientation : YZ")
+        extent[0] = sliceno
+        extent[1] = sliceno + 1
+    voi.SetVOI(extent)
+    voi2.SetVOI(extent)
+    voi.Update()
+    voi2.Update()
+    subtract.Update()
+
+    extentc = list(subtract.GetOutput().GetExtent())
+    if orientation == SLICE_ORIENTATION_XY:
+        print ("slice orientation : YZ")
+        extentc[4] = sliceno
+        extentc[5] = sliceno + 1
+    elif orientation == SLICE_ORIENTATION_XZ:
+        print ("slice orientation : YZ")
+        extentc[2] = sliceno
+        extentc[3] = sliceno + 1
+
+    elif orientation == SLICE_ORIENTATION_YZ:
+        print ("slice orientation : YZ")
+        extentc[0] = sliceno
+        extentc[1] = sliceno + 1
+
+    data.CopyAndCastFrom(subtract.GetOutput(), *extentc)
+    # dims = data.GetDimensions()
+    # im1 = numpy.reshape(
+    #         numpy_support.vtk_to_numpy(
+    #                     data.GetPointData().GetScalars()
+    #                     ),
+    #         (dims[0],dims[1],dims[2]), order='F'
+    #     )
+    # dims = subtract.GetOutput().GetDimensions()
+    # im2 = numpy.reshape(
+    #     numpy_support.vtk_to_numpy(
+    #                 subtract.GetOutput().GetPointData().GetScalars()
+    #                 ),
+    #     (dims[0],dims[1],dims[2]), order='F'
+    # )
+    # if orientation == SLICE_ORIENTATION_XY:
+    #     print ("slice orientation : YZ")
+    #     im1[sliceno,:,:] = im2
+    # elif orientation == SLICE_ORIENTATION_XZ:
+    #     print ("slice orientation : YZ")
+    #     im1[:,sliceno,:] = im2
+
+    # elif orientation == SLICE_ORIENTATION_YZ:
+    #     print ("slice orientation : YZ")
+    #     im1[:,:,sliceno] = im2
+    #v.setInputData(subtract.GetOutput())
+    print ("data" , data.GetDimensions(), data.GetExtent())
+    print ("subtract", subtract.GetOutput().GetDimensions(), subtract.GetOutput().GetExtent())
+    v.setInputData(data)
+    v.style.UpdatePipeline()
 
 
 #%%
@@ -252,43 +349,84 @@ if __name__ == '__main__':
     err.SetFileName("viewer.log")
     vtk.vtkOutputWindow.SetInstance(err)
 
-    reader = vtk.vtkMetaImageReader()
-    reader.SetFileName('../../../../CCPi-Simpleflex/data/head.mha')
-    reader.Update()
-    extent = reader.GetOutput().GetExtent()
+    #v.style.AddObserver('MouseWheelForwardEvent', OnSliceEvent, 0.5)
+    #v.style.AddObserver('MouseWheelBackwardEvent', OnSliceEvent, 0.5)
 
+    if len(sys.argv) >= 3:
+        fname1 = os.path.abspath(sys.argv[1])
+        fname2 = os.path.abspath(sys.argv[2])
+    else:
+        fname1 = '../../../../CCPi-Simpleflex/data/head.mha'
+        fname2 = fname1
+    print ("Reading image 1")
+    reader = vtk.vtkMetaImageReader()
+    reader.SetFileName(fname1)
+    reader.Update()
+    print ("Done")
+    extent = (200, 700, 200, 700, 200, 500)
+    
+    print ("Extracting selection")
     voi = vtk.vtkExtractVOI()
     voi.SetInputData(reader.GetOutput())
-    voi.SetVOI(extent[1]//10, int(extent[1] *(0.9)),extent[3]//10, int(extent[3] *(0.9)) , extent[4], extent[5])
+    voi.SetVOI(*extent)
     voi.Update()
+    print ("Done")
 
-    translate = vtk.vtkImageTranslateExtent()
-    translate.SetTranslation(5,-5,0)
-    translate.SetInputData(voi.GetOutput())
-    translate.Update()
+    #voi = reader
+    data1 = vtk.vtkImageData()
+    data1.DeepCopy(voi.GetOutput())
+    
+    print ("Reading image 2")
+    reader.SetFileName(fname2)
+    reader.Update()
+    print ("Extracting selection")
+    voi.Update()
+    print ("Done")
 
+    data2 = vtk.vtkImageData()
+    data2.DeepCopy(voi.GetOutput())
+    print ("clearing memory")
+    del voi
+    del reader
+    print ("clearing memory done")
+
+    v = CILViewer2D()
+    
+    if len(sys.argv) >= 4:
+        if sys.argv[3] == 'translate':
+            #voi = reader
+            print("translate")
+            translate = vtk.vtkImageTranslateExtent()
+            translate.SetTranslation(0,0,0)
+            translate.SetInputData(data2)
+            translate.Update()
+
+            v.style.AddObserver('KeyPressEvent', OnKeyPressEvent, 0.5)
+        elif sys.argv[3] == 'transform':
+            # doesn't work
+            print("transform")
+            translate = vtk.vtkTransformFilter()
+            transform = vtk.vtkTransform()
+            transform.Translate(0,0,0)
+            translate.SetInputData(data2)
+            translate.SetTransform(transform)
+            translate.Update()
+            print (translate.GetOutput().GetDimensions())
+            v.style.AddObserver('KeyPressEvent', OnKeyPressEventTransform, 0.5)
+        else:
+            raise ValueError('no method')
+    else:
+        print ("no method specified")
 
 
 
     # print ("out of the reader", reader.GetOutput())
-    if False:
-        wildcard_filenames2 = '204507_8bit/*.tif'
-        filenames2 = glob.glob(wildcard_filenames2)
-
-        reader2 = vtk.vtkTIFFReader()
-        sa2 = vtk.vtkStringArray()
-        for fname in filenames2:
-            i = sa2.InsertNextValue(fname)
-
-        print("read {} files".format(i))
-
-        reader2.SetFileNames(sa)
-        reader2.Update()
+    
     use_vtk = True
     if use_vtk:
         cast1 = vtk.vtkImageCast()
         cast2 = vtk.vtkImageCast()
-        cast1.SetInputConnection(voi.GetOutputPort())
+        cast1.SetInputData(data1)
         cast1.SetOutputScalarTypeToFloat()
         cast2.SetInputConnection(translate.GetOutputPort())
         cast2.SetOutputScalarTypeToFloat()
@@ -302,6 +440,7 @@ if __name__ == '__main__':
         
         subtract.Update()
     else:
+        # this won't work as the shape of the image is not constant
         subtract = cilImageMathematics()
         subtract.SetOperation(cilImageMathematics.SUBTRACT)
         #subtract.SetOperation('pippo')
@@ -310,16 +449,14 @@ if __name__ == '__main__':
         subtract.SetInputConnection(1,translate.GetOutputPort())
         subtract.Update()
 
-    print ("subtract type", subtract.GetOutput().GetScalarTypeAsString())
+    print ("subtract type", subtract.GetOutput().GetScalarTypeAsString(), subtract.GetOutput().GetDimensions())
     
     stats = vtk.vtkImageHistogramStatistics()
     stats.SetInputConnection(subtract.GetOutputPort())
     stats.Update()
     print ("stats ", stats.GetMinimum(), stats.GetMaximum(), stats.GetMean(), stats.GetMedian())
     
-    v = CILViewer2D()
-
-    v.style.AddObserver('KeyPressEvent', OnKeyPressEvent, 0.5)
+    
 
     v.setInputData(subtract.GetOutput())
     v.startRenderLoop()
