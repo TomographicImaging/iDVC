96e1a60fda6522a3ce30d18f48a4b1c6db832f6d
paskino <edo.paskino@gmail.com>
2021-03-17 21:50:53 +0000

    fix OSX build

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -11,10 +11,30 @@ if [ `python -c "from __future__ import print_function; import platform; print (
 then 
   echo "Darwin"; 
   
-  cmake ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=OFF -DCONDA_BUILD=ON -DBUILD_CUDA=OFF -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}/Library" -DOPENMP_INCLUDES=${CONDA_PREFIX}/include -DOPENMP_LIBRARIES=${CONDA_PREFIX}/lib
+  #cmake ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=OFF -DCONDA_BUILD=ON -DBUILD_CUDA=OFF -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB=${CONDA_PREFIX}/lib -DLIBRARY_INC=${CONDA_PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX}/Library -DOPENMP_INCLUDES=${CONDA_PREFIX}/include -DOPENMP_LIBRARIES=${CONDA_PREFIX}/lib
+  cmake ${RECIPE_DIR}/../../../  \
+                        -DBUILD_PYTHON_WRAPPER=OFF\
+                        -DCMAKE_BUILD_TYPE="Release"\
+                        -DLIBRARY_LIB=$CONDA_PREFIX/lib \
+                        -DLIBRARY_INC=$CONDA_PREFIX \
+                        -DCMAKE_INSTALL_PREFIX=$PREFIX\
+                        -DOPENMP_CXX_INCLUDE_DIR=${CONDA_PREFIX}/include \
+                        -DOPENMP_LIBRARIES=${CONDA_PREFIX}/lib
+#-DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I${CONDA_PREFIX}/include" \
+#-DOpenMP_C_LIB_NAMES="omp" \
+#-DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I${CONDA_PREFIX}/include" \
+ #               -DOpenMP_CXX_LIB_NAMES="omp" \
+ #               -DOpenMP_omp_LIBRARY=${CONDA_PREFIX}/lib/libomp.dylib
+
 else 
   echo "something else"; 
-  cmake ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=OFF -DCONDA_BUILD=ON -DBUILD_CUDA=OFF -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}/Library" 
+  #cmake ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=OFF -DCONDA_BUILD=ON -DBUILD_CUDA=OFF -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}/Library" 
+  cmake ${RECIPE_DIR}../../../  \
+                        -DBUILD_PYTHON_WRAPPER=OFF\
+                        -DCMAKE_BUILD_TYPE="Release"\
+                        -DLIBRARY_LIB=$CONDA_PREFIX/lib \
+                        -DLIBRARY_INC=$CONDA_PREFIX \
+                        -DCMAKE_INSTALL_PREFIX=$PREFIX
 fi
 cmake --build . --target install
 ${PYTHON} setup.py install
