e94207d6f2277ff1587f95276e3040fded87dba3
Edoardo Pasca <edo.pakino@gmail.com>
2021-03-17 14:45:53 +0000

    add build script for unix

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -9,3 +9,15 @@ cd $SRC_DIR/ccpi
 $PYTHON setup.py install
 
 
+
+mkdir ${SRC_DIR}/ccpi
+cp -r ${RECIPE_DIR}/../src ${SRC_DIR}/ccpi/src
+ROBOCOPY /E ${RECIPE_DIR}/../ccpi ${SRC_DIR}/ccpi/ccpi
+copy ${RECIPE_DIR}/../setup.py ${SRC_DIR}/ccpi
+cd ${SRC_DIR}/ccpi
+
+#:: issue cmake to create setup.py
+cmake -G "Unix Makefiles" ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=OFF -DCONDA_BUILD=ON -DBUILD_CUDA=OFF -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}/Library" 
+
+cmake --build . --target install
+${PYTHON} setup.py install
