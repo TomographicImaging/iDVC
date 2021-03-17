d31d5e8437c62bfb1640d0ea1ed87881f99eff95
Edoardo Pasca <edo.pakino@gmail.com>
2021-03-17 14:49:44 +0000

    add build script different for osx and linux

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -17,7 +17,14 @@ copy ${RECIPE_DIR}/../setup.py ${SRC_DIR}/ccpi
 cd ${SRC_DIR}/ccpi
 
 #:: issue cmake to create setup.py
-cmake -G "Unix Makefiles" ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=OFF -DCONDA_BUILD=ON -DBUILD_CUDA=OFF -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}/Library" 
-
+if [ `python -c "from __future__ import print_function; import platform; print (platform.system())"`  == "Darwin" ] ;
+then 
+  echo "Darwin"; 
+  
+  cmake ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=OFF -DCONDA_BUILD=ON -DBUILD_CUDA=OFF -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}/Library" -DOPENMP_INCLUDES=${CONDA_PREFIX}/include -DOPENMP_LIBRARIES=${CONDA_PREFIX}/lib
+else 
+  echo "something else"; 
+  cmake ${RECIPE_DIR}/../../../ -DBUILD_PYTHON_WRAPPERS=OFF -DCONDA_BUILD=ON -DBUILD_CUDA=OFF -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}/Library" 
+fi
 cmake --build . --target install
 ${PYTHON} setup.py install
