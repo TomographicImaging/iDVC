d130001c6fb012adf0e85a197d11c52313443e7d
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-05-25 20:41:02 +0100

    Fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,8 @@
 ::)
 
 mkdir "%SRC_DIR%"
-copy "%RECIPE_DIR%\.." "%SRC_DIR%"
+ROBOCOPY /E "%RECIPE_DIR%\..\src\ccpi" "%SRC_DIR%\ccpi"
+copy "%RECIPE_DIR%\..\setup.py" "%SRC_DIR%\setup.py"
 cd %SRC_DIR%
 
 %PYTHON% setup.py install
