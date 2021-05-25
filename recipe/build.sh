9d6e8c139d4c2e5b63edc5106a3fba7b24b7d58d
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-05-25 17:48:31 +0100

    Fix filepaths

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 set -ex
 mkdir -p $SRC_DIR
-cp -rv $RECIPE_DIR/../../Apps/DVC_configurator/ccpi $SRC_DIR
-cp -rv $RECIPE_DIR/../../Apps/DVC_configurator/setup.py $SRC_DIR/setup.py
+cp -rv $RECIPE_DIR/../src/ccpi $SRC_DIR
+cp -rv $RECIPE_DIR/../setup.py $SRC_DIR/setup.py
 cd ${SRC_DIR}
 
 ${PYTHON} setup.py install
