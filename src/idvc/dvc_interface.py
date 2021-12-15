43d680e2cac221fc57f9500bafe009b7965295de
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-12-15 14:33:33 +0000

    Fixes error in displaying DVC results (#78)
    * Remove underscore in result folder name
    
    * reinstate imports

--- src/idvc/dvc_interface.py
+++ src/idvc/dvc_interface.py
@@ -3992,7 +3992,7 @@ This parameter has a strong effect on computation time, so be careful."
                                message="Load a mask on the viewer first" )
                 return
         
-        folder_name = "_" + self.rdvc_widgets['name_entry'].text()
+        folder_name = self.rdvc_widgets['name_entry'].text()
 
         results_folder = os.path.join(tempfile.tempdir, "Results")
 
@@ -4094,7 +4094,7 @@ This parameter has a strong effect on computation time, so be careful."
         os.chdir(tempfile.tempdir)
         progress_callback = kwargs.get('progress_callback', None)
         try:
-            folder_name = "_" + self.rdvc_widgets['name_entry'].text()
+            folder_name = self.rdvc_widgets['name_entry'].text()
 
             results_folder = os.path.join(tempfile.tempdir, "Results")
             os.mkdir(os.path.join(results_folder, folder_name))
