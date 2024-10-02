dbf8472fe4ae3b558fc68e66779882d153ede9ca
Danica Sugic <danica.sugic@stfc.ac.uk>
2024-10-02 17:26:07 +0100

    Add docstrings to manipulate results files

--- src/idvc/utils/manipulate_result_files.py
+++ src/idvc/utils/manipulate_result_files.py
@@ -4,24 +4,63 @@ from idvc.pointcloud_conversion import PointCloudConverter
 from idvc.utilities import RunResults
 import glob, os
 
-def extractDataFromDispResultFile(result, displ_wrt_point0):    
+def _extractDataFromDispResultFile(result, displ_wrt_point0):
     """
-    Gets the filepath of the disp file via `result.disp_file`.
-    Extracts the data in the numpy format, skips the header.
-    This imports the whole row, so the displacement vector is given by indices 6, 7, 8. Index 5 is the objective func minimum.
-    'plot_data' is a list of array, where each array is a column of data: objmin, u, v, w.
+    Extracts objective minimum and displacement vectors from a result file.
+    Optionally, adjusts the displacement vectors relative to the displacement of the first point.
+
+    The objective function minimum is located at index 5. 
+    The displacement vector is extracted from columns indexed 6 to 8 (inclusive).
+    If `displ_wrt_point0` is True, the displacement values will be adjusted by
+    subtracting the displacement of the first point.
+
+    Parameters
+    ----------
+    result : RunResults
+        An object containing the filepath to the displacement result file (`result.disp_file`).
+        The displacement result file is expected to be tab-delimited and have a header row that will be skipped.
+    displ_wrt_point0 : bool
+        If True, the displacement vectors will be adjusted relative to the displacement vector of the first point
+        (point zero).
+
+    Returns
+    -------
+    numpy.ndarray
+        2D array where each row corresponds to a column of data from the file,
+        including the objective function minimum and displacement vectors.
     """
     data = np.genfromtxt(result.disp_file, delimiter='\t', skip_header=1)
     data_shape = data.shape
     index_objmin = 5
-    index_disp = [6,9]
+    index_disp = [6, 9]
     if displ_wrt_point0:
-        point0_disp_array = data[0,index_disp[0]:index_disp[1]]
-        data[:,index_disp[0]:index_disp[1]] = data[:,index_disp[0]:index_disp[1]] - point0_disp_array
-    result_arrays = np.transpose(data[:,index_objmin:data_shape[1]])
+        point0_disp_array = data[0, index_disp[0]:index_disp[1]]
+        data[:, index_disp[0]:index_disp[1]] -= point0_disp_array
+    result_arrays = np.transpose(data[:, index_objmin:data_shape[1]])
     return result_arrays
 
 def createResultsDataFrame(results_folder, displ_wrt_point0):
+    """
+    Creates a pandas DataFrame containing results from DVC result files.
+    The folder is scanned for subfolders matching the pattern
+    "dvc_result_*". The data for a result is extracted from each result file, and compiles
+    the data into a pandas DataFrame.
+
+    Parameters
+    ----------
+    results_folder : str
+        The path to the folder containing the DVC result subfolders.
+    displ_wrt_point0 :bool
+        A flag indicating whether to extract displacement data with respect to point 0.
+    
+    Returns
+    -------
+    pd.DataFrame: A DataFrame with the following columns:
+            - 'subvol_size': List of subvolume sizes as strings.
+            - 'subvol_points': List of subvolume points as strings.
+            - 'result': List of RunResults objects.
+            - 'result_arrays': List of 4 arrays containing extracted data.
+    """
     subvol_size_list = []
     subvol_points_list = []
     result_list = []
@@ -29,7 +68,7 @@ def createResultsDataFrame(results_folder, displ_wrt_point0):
 
     for folder in glob.glob(os.path.join(results_folder, "dvc_result_*")):
         result = RunResults(folder)
-        result_arrays = extractDataFromDispResultFile(result, displ_wrt_point0)
+        result_arrays = _extractDataFromDispResultFile(result, displ_wrt_point0)
         subvol_size_list.append(str(result.subvol_size))
         subvol_points_list.append(str(result.subvol_points))
         result_list.append(result)
@@ -43,6 +82,22 @@ def createResultsDataFrame(results_folder, displ_wrt_point0):
     return result_data_frame
 
 def addMeanAndStdToResultDataFrame(result_data_frame):
+    """
+    Adds mean and standard deviation arrays to the result DataFrame.
+
+    In particular, iterates over each row in the DataFrame, calculates the mean and 
+    standard deviation for each array in the 'result_arrays' column, and appends
+    the new columns 'mean_array' and 'std_array'.
+
+    Parameters
+    ----------
+    result_data_frame : pd.DataFrame
+        A pandas DataFrame.
+
+    Returns
+    -------
+    pd.DataFrame: The modified DataFrame with additional columns 'mean_array' and 'std_array'.
+    """
     mean_array_list = []
     std_array_list = []
     for row in result_data_frame.itertuples():
